1ede8b8306f22b497646798f3fc1279d95f8d995
Maneesh Kumar <maneesh.kumar@HFCL.COM>
2023-02-21 19:12:36 +0530

    Device registration/Push notification

--- screen/regDeviceScreen.js
+++ screen/regDeviceScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {View, Text, StyleSheet, Image, Alert} from 'react-native'
+import React, { useEffect, useRef, useState } from "react";
+import {View, Text, StyleSheet, Image, Alert, Platform, Button} from 'react-native'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { globalStyles } from "../style/globalstyle";
 import HeaderCardComponent from "../component/card/headerCard";
@@ -8,14 +8,113 @@ const logoIcon = require('../assets/icons/logo.png')
 import { MaterialIcons } from '@expo/vector-icons';
 import  Constants  from "expo-constants";
 import LoadingDialogComponent from "../component/loadingDialogComponent";
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+
+
+Notifications.setNotificationHandler({
+    handleNotification: async() =>({
+        shouldShowAlert:true,
+        shouldPlaySound:false,
+        shouldSetBadge:false
+    })
+})
+
+async function sendPushNotification(expoPushToken){
+    const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Original Title',
+        body: 'And here is the body!',
+        data: { someData: 'goes here' },
+      };
+      console.log(message)
+      await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-encoding': 'gzip, deflate',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+      });
+    }
+    
+
+
+
+
+async function registerForPushNotificationAsync(){
+    let token;
+    if (Device.isDevice){
+        const{status:existingStatus} = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus
+        if( existingStatus !=='granted')
+        {
+            const{status} = await Notifications.getPermissionsAsync();
+            finalStatus = status
+        }
+        
+        if(finalStatus !=='granted'){
+            Alert.alert('failed to get notification');
+           // return;
+        }
+        console.log(finalStatus)
+        token = (await Notifications.getExpoPushTokenAsync()).data
+        console.log(token)
+    }
+    else{
+        console.log('Must use physical device for Notification')
+        Alert.alert('Must use physical device for Notification')
+    }
+
+    if(Platform.OS === 'android'){
+        Notifications.setNotificationChannelAsync('default',{
+            name:'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0,250,250,250],
+            lightColor:'#FF231F7C',
+            enableLights:true
+        })
+    }
+
+    return token
+}
 
 const RegDeviceScreen = ({navigation})=>{
     const[isLoading, setIsLoading] = useState(true)    
-
+    const[expoPushToken, setExpoPushToken] = useState('')
+    const[notification, setNotification] = useState(false)
+    const notificationListener = useRef()
+    const responseListener = useRef()
 
     useEffect(()=>{
+  
+        
+        registerForPushNotificationAsync().then(token => setExpoPushToken(token))
+        
+        notificationListener.current = Notifications.addNotificationReceivedListener(
+            ()=> setNotification(notification)
+        )
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(
+            reponse => console.log(reponse)
+        )
+
         getRegDeviceId()
+
+        return ()=>{
+            Notifications.removeNotificationSubscription(notificationListener.current)
+            Notifications.removeNotificationSubscription(responseListener.current)
+        }
+
+        
+
     },[])
+
+    const notifySettings = async ()=>{
+        
+    }
 
     const getRegDeviceId = async()=>{
         const value = await AsyncStorage.getItem("@reg_dev")
@@ -74,6 +173,23 @@ const RegDeviceScreen = ({navigation})=>{
 
                     }
                 </View>
+
+                <View>
+                    <Text>Your Expo push token: {expoPushToken}</Text>
+                    <View style={{alignItems:'center', justifyContent:'center'}}>
+                        <Text>Title: {notification && notification.request.content.title} </Text>
+                        <Text>Body: {notification && notification.request.content.body}</Text>
+                        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+                    </View>
+                </View> 
+                <Button title="Press to send Notification"
+                    onPress={async()=>{
+                        await sendPushNotification(expoPushToken)
+
+                    }}
+                >
+
+                </Button>
          </View>
     )
 }
